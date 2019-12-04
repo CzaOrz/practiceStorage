b3a53bcd8ce42b1cc0881cd44d234d8f0cfa858b
cza <chenziangsg@163.com>
2019-12-04 21:09:28 +0800

    update

--- 数据库/mysql/sql练习.sql
+++ 数据库/mysql/sql练习.sql
@@ -10,6 +10,10 @@
 -- DISTINCT <select_list>
 -- ORDER BY <order_by_condition>
 -- LIMIT <limit_number> OFFSET <offset_number>
+-- SUM
+-- AVG
+-- MAX
+-- MIN
 
 -- INNER JOIN 只返回同时存在于两张表的行数据
 -- RIGHT OUTER JOIN 返回右表都存在的行
@@ -43,4 +47,30 @@ id	name
 SELECT * FROM students;
 SELECT COUNT(*) sum FROM students;
 SELECT class_id std_id FROM students WHERE gender = 'M';
-SELECT * FROM students ORDER BY score DESC; -- ??? why not not work
+SELECT * FROM students ORDER BY score DESC; -- why not not work. must appoint column?
+SELECT * FROM students LIMIT 3 OFFSET 0;
+SELECT CEILING(COUNT(*)/3) FROM students;
+SELECT COUNT(*) FROM students GROUP BY gender;
+SELECT students.id students_id, classes.id classes_id FROM students, classes;
+SELECT *
+FROM students s
+INNER JOIN classes c
+ON s.id = c.id;
+
+
+-- INSERT
+INSERT INTO students (class_id, name, gender, score) VALUES (1, 'cza', 'M', 24);
+INSERT INTO students (class_id, name, gender, score) VALUES
+(1, 'cza', 'M', 24),
+(1, 'cza', 'M', 24);
+
+
+--UPDATE
+UPDATE students SET name = 'cza', score = 100 WHERE id = 1;
+UPDATE students SET score=score+1 WHERE score < 98;
+UPDATE students SET score = 59;
+
+
+--DELETE
+DELETE FROM students WHERE name = 'cza';
+DELETE FROM students;
