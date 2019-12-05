244ade2cac045b1edcff0dff445943b8feabdf44
cza <czasg0.0>
2019-12-05 10:25:08 +0800

    update

--- 数据库/mysql/sql练习.sql
+++ 数据库/mysql/sql练习.sql
@@ -42,6 +42,19 @@ id	name
 4	ËÄ°à
 */
 
+-- STRUCT OF TABLE
+DESC students;
+SHOW CREATE TABLE students;
+ALTER TABLE students ADD COLUMN birth VARCHAR(20) NOT NULL;
+ALTER TABLE students CHANGE COLUMN birth birthday VARCHAR(30) NOT NULL;
+ALTER TABLE students DROP COLUMN birthday;
+
+-- EASE SQL
+REPLACE INTO students (id, class_id, name, gender, score) VALUES (1, 1, 'cza', 'M', 24);  -- insert one record or replace old one if exist
+INSERT INTO students (id, class_id, name, gender, score) VALUES (1, 1, 'cza', 'M', 24) ON DUPLICATE KEY UPDATE name='cza', gender='F', score=22;  -- insert one record or update it if exist;
+INSERT IGNORE INTO students (id, class_id, name, gender, score) VALUES (1, 1, 'cza', 'M', 24);
+CREATE TABLE students_fast SELECT * FROM students WHERE class_id=1;
+
 
 -- SELECT
 SELECT * FROM students;
@@ -51,6 +64,7 @@ SELECT * FROM students ORDER BY score DESC; -- why not not work. must appoint co
 SELECT * FROM students LIMIT 3 OFFSET 0;
 SELECT CEILING(COUNT(*)/3) FROM students;
 SELECT COUNT(*) FROM students GROUP BY gender;
+SELECT COUNT(DISTINCT name) FROM students WHERE score>90;  -- by this way, you can filter by WHERE first, then get the num of all records
 SELECT students.id students_id, classes.id classes_id FROM students, classes;
 SELECT *
 FROM students s
@@ -80,6 +94,16 @@ DROP database if EXISTS cza_test;
 CREATE database cza_test;
 use cza_test;
 CREATE TABLE students (
-    `test_datetime` DATETIME NOT NULL
+    `id` bigint NOT NULL auto_increment,
+    `class_id` bigint NOT NULL,
+    `name` varchar(10) NOT NULL,
+    `gender` varchar(5) NOT NULL,
+    `score` int NOT NULL,
+    primary key (`id`),
+    key `idx_name` (`name`),
+    unique key `idx_name` (`name`),
 ) engine=innodb default charset=utf8;
 
+
+mysql -u root -p
+mysql -h 127.0.0.1 -u root -p
