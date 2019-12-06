1b0f0e06c4bde48066336c48c9f92c8723c8bc1c
cza <chenziangsg@163.com>
2019-12-06 22:55:52 +0800

    update

--- 后端/super接口汇总/learn_tornado/api.py
+++ 后端/super接口汇总/learn_tornado/api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import json
 import tornado.ioloop
 import tornado.web
 
@@ -28,8 +29,67 @@ class API03(tornado.web.RequestHandler):
 
 class API04(tornado.web.RequestHandler):
     def post(self):
-        form = self.request.query_arguments
-        self.write({'status': 1, 'cza': 'cza', 'form': form})
+        form = self.request.body_arguments
+        print(self.request.arguments == self.request.body_arguments)
+        self.get_argument('cza')  # post请求参数也可以通过get_argument获取
+        self.get_arguments('cza')  # type: list
+        self.write({'status': 1, 'form': f"{form}"})
+
+
+class API05(tornado.web.RequestHandler):
+    def post(self):
+        json_data = json.loads(self.request.body.decode())
+        self.write({'status': 1, 'json': f"{json_data}"})
+
+
+class API06(tornado.web.RequestHandler):
+    def get(self):
+        cookie = self.get_cookie('cza')
+        self.write({'status': 1, 'cookies': f"{cookie}"})
+
+
+class API07(tornado.web.RequestHandler):
+    def post(self):
+        cookie = self.get_cookie('cza')
+        self.write({'status': 1, 'cookies': f"{cookie}"})
+
+
+class API08(tornado.web.RequestHandler):
+    def get(self): raise tornado.web.HTTPError(404)
+
+
+class API09(tornado.web.RequestHandler):
+    def get(self):
+        self.set_status(404)
+        self.render('errors/error_404.html', error_text="error_text_lol", error_code=404)
+
+
+class API10(tornado.web.RequestHandler):
+    def post(self): raise tornado.web.HTTPError(404)
+
+
+class API11(tornado.web.RequestHandler):
+    def post(self):
+        self.set_status(404)
+        self.render('errors/error_404.html', error_text="error_text_lol", error_code=404)
+
+
+class API12(tornado.web.RequestHandler):
+    def get(self):
+        self.render('test/index.html', remarks="fucking")
+
+
+class API13(tornado.web.RequestHandler):
+    def get(self): self.redirect('/')
+
+
+class API14(tornado.web.RequestHandler):
+    def post(self):
+        files = self.request.files['file']
+        for file in files:
+            filename = file['filename']
+            with open(filename, 'wb') as f:
+                f.write(file['body'])
 
 
 if __name__ == '__main__':
@@ -44,13 +104,23 @@ if __name__ == '__main__':
             (r"/02/{0,1}([^/]*)?", API02),  # 似乎一不下心就被写成动态path了
             (r"/03", API03),
             (r"/04", API04),
+            (r"/05", API05),
+            (r"/06", API06),
+            (r"/07", API07),
+            (r"/08", API08),
+            (r"/09", API09),
+            (r"/10", API10),
+            (r"/11", API11),
+            (r"/12", API12),
+            (r"/13", API13),
+            (r"/14", API14),
         ],
         cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         # 同源策略 - 所谓同源是指，域名，协议，端口相同。 不同源的客户端脚本(javascript、ActionScript)在没明确授权的情况下，不能读写对方的资源
         # 第三方站点没有访问cookie数据的权限（同源策略）所以我们可以要求每个请求包括一个特定的参数值（XSRF）作为令牌来匹配存储在cookie中的对应值
-        xsrf_cookies=True,
+        # xsrf_cookies=True,
         # debug=options.debug,
     )
     app.listen(options.port)
--- 后端/super接口汇总/test_for_api.py
+++ 后端/super接口汇总/test_for_api.py
@@ -3,7 +3,7 @@ import requests
 URL = "http://127.0.0.1:8888/"
 
 
-def test4(): print(requests.post(f"{URL}04", data={'cz': 'cza'}).text)
+def test4(): print(requests.post(f"{URL}04", data={'cz': 'cza', 'cza': 'cza'}).text)
 
 
 def test5(): print(requests.post(f"{URL}05", json={'cz': 'cza'}).json())
@@ -18,17 +18,22 @@ def test7(): print(requests.post(f"{URL}07", cookies={'cza': 'hei', 'man': 'are 
 def test8(): print(requests.get(f"{URL}08").status_code)
 
 
-def test9(): print(requests.get(f"{URL}09").text)
+def test9():
+    print(requests.get(f"{URL}09").text)
+    print(requests.get(f"{URL}09").status_code)
 
 
 def test10(): print(requests.post(f"{URL}10").text)
+
+
+def test11(): print(requests.post(f"{URL}11").text)
 
 
 def test14():
     files = {'file': open('cza.xls', 'rb')}
     files = {'file': ('cza.xls', open('cza.xls', 'rb'), 'application/vnd.ms-excel', {'Expires': '0'})}
     files = {'file': ('fileName.txt', 'file content\nfile body')}
-    print(requests.post(f"{URL}14", files=files).json())
+    print(requests.post(f"{URL}14", files=files).text)
 
 
 def test15(): print(requests.get(f"{URL}15", cookies={'cza': 'cza-is-sg', 'orz': 'cza-is-sg'}).status_code)
@@ -41,15 +46,15 @@ def test17(): print(requests.options(f"{URL}17", cookies={'cza': 'cza-is-sg', 'o
 
 
 if __name__ == '__main__':
-    test4()
+    # test4()
     # test5()
     # test6()
     # test7()
     # test8()
     # test9()
     # test10()
-    # test14()
+    # test11()
+    test14()
     # test15()
     # test16()
     # test17()
-
