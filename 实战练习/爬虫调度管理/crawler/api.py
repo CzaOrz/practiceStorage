114ca4c7acc35d103a6cbb5ab14d393babf8cb30
cza <czasg0.0>
2019-12-26 18:01:31 +0800

    update project

--- 实战练习/爬虫调度管理/crawler/api.py
+++ 实战练习/爬虫调度管理/crawler/api.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from minitools.db.mongodb import get_mongodb_client
 from minitools import timekiller
 from flask import jsonify
@@ -8,6 +9,16 @@ mongodb_ziru_statistics = get_mongodb_client()["housePrice"]["ziru_zufang_statis
 
 
 @bp_crawler.route("/api/ziru/data")
-def api_ziru():
-    doc = mongodb_ziru_statistics.find_one({"timestamp": timekiller.get_today().timestamp()}, {"_id": 0})
-    return jsonify(doc)
+@bp_crawler.route("/api/ziru/data/<int:day>")
+def api_ziru(day=15):
+    # 1、需要总量，然后相减，画出近15日统计增量折线图
+    # 2、需要各地域的数据，左边诶总量，右边为日增量
+    documents = mongodb_ziru_statistics.find(
+        {"timestamp": {"$gte": timekiller.get_past_day(day).timestamp()}}, {"_id": 0})
+    documents = list(documents)
+    documents.sort(key=lambda di: di["timestamp"])
+    count_statistics = [document["all_count"] for document in documents]
+    return jsonify({
+        "status": 0,
+        "ziru": documents,
+    })
