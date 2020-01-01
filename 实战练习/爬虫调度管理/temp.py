f240e4c2da021074ee589aa0810d9b95f4318135
cza <chenziangsg@163.com>
2020-01-02 00:36:06 +0800

    update

--- 实战练习/爬虫调度管理/temp.py
+++ 实战练习/爬虫调度管理/temp.py
@@ -16,3 +16,41 @@
 # print(requests.post("http://127.0.0.1:8867/scheduler/jobs/test_1/resume").text)
 
 # print(requests.delete("http://127.0.0.1:8867/scheduler/jobs/test_1").text)
+
+
+# import time
+# import socket
+#
+# import threading
+# import select
+# import selectors
+#
+# soc = socket.socket()
+# soc.bind(("127.0.0.1", 8866))
+# soc.listen(3)
+#
+# socks = {}
+#
+#
+# def test():
+#     while True:
+#         if socks:
+#             so = socks["test"]
+#             try:
+#                 so.setblocking(False)
+#                 print(so.recv(1024))
+#             except (BlockingIOError, socket.timeout):
+#                 print("报错了")
+#             except ConnectionError:
+#                 print("估计连接断开了")
+#
+#         print("pass")
+#         time.sleep(5)
+#
+#
+# threading.Thread(target=test).start()
+#
+# while True:
+#     sock, address = soc.accept()
+#     name = sock.recv(1024).decode()
+#     socks[name] = sock
